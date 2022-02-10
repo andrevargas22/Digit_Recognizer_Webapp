39e48c6d77e2b52bee346fb43d1acfad57a921e0
Andre Vargas <andrevargas22@gmail.com>
2022-02-10 14:30:01 -0300

    updated README and train code

---
+++ README.md
@@ -0,0 +1,15 @@
+# Digit Recognizer Webapp
+
+This repository contains a small web app that runs a MNIST trained model for handwritten digit recognition.
+
+### Install Requirements:
+
+```
+> pip install requirements.txt
+```
+
+### Run train.py to generate the model Digit_Recognition_Model.h5:
+```
+> python train.py
+```
+
--- train.py
+++ train.py
@@ -1,1 +1,52 @@
-print('hello world!')
+# This is MNIST digit classifier model trained in TensorFlow and Keras
+
+# Import Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# Load the MNIST dataset
+mnist = tf.keras.datasets.mnist
+train_data, test_data = mnist.load_data()
+
+# Divide the data into features and labels
+train_images, train_labels = train_data
+test_images, test_labels = test_data
+
+# Reshape and normalize the images
+X_train = train_images.reshape((60000, 28, 28, 1))
+X_train = X_train.astype('float32') / 255
+X_test = test_images.reshape((10000, 28, 28, 1))
+X_test = X_test.astype('float32') / 255
+
+# Reshape the labels and encode them categorically
+y_train = tf.keras.utils.to_categorical(train_labels)
+y_test = tf.keras.utils.to_categorical(test_labels)
+
+# Define the sequential model
+model = tf.keras.models.Sequential()
+
+# Define the convolutional neural network
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(2, 2))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Define the parameters
+num_epochs = 10
+batch_size = 128
+
+# Train the model
+history = model.fit(X_train, y_train, epochs=num_epochs, batch_size=batch_size, validation_data=(X_test, y_test))
+
+# Save the model to be used in the web app
+model.save('Digit_Recognition_Model.h5')
+
+
