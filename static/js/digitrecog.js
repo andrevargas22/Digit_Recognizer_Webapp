3225737157e2013accc47e47b9121d6ab614703f
Andre Vargas <andrevargas22@gmail.com>
2022-02-10 15:44:35 -0300

    added prediction

--- static/js/digitrecog.js
+++ static/js/digitrecog.js
@@ -11,108 +11,169 @@ var mousePos = { x:0, y:0 };
 var lastPos = mousePos;
 
 canvas.addEventListener("mousedown", function (e) {
-        drawing = true;
-  lastPos = getMousePos(canvas, e);
+    drawing = true;
+    lastPos = getMousePos(canvas, e);
 }, false);
+
 canvas.addEventListener("mouseup", function (e) {
-  drawing = false;
+    drawing = false;
 }, false);
+
 canvas.addEventListener("mousemove", function (e) {
-  mousePos = getMousePos(canvas, e);
+    mousePos = getMousePos(canvas, e);
 }, false);
 
 // Get the position of the mouse relative to the canvas
 function getMousePos(canvasDom, mouseEvent) {
-  var rect = canvasDom.getBoundingClientRect();
-  return {
-    x: mouseEvent.clientX - rect.left,
-    y: mouseEvent.clientY - rect.top
-  };
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+        x: mouseEvent.clientX - rect.left,
+        y: mouseEvent.clientY - rect.top
+    };
 }
 
 // Get a regular interval for drawing to the screen
 window.requestAnimFrame = (function (callback) {
-  return window.requestAnimationFrame ||
-     window.webkitRequestAnimationFrame ||
-     window.mozRequestAnimationFrame ||
-     window.oRequestAnimationFrame ||
-     window.msRequestAnimaitonFrame ||
-     function (callback) {
-  window.setTimeout(callback, 1000/60);
-     };
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimaitonFrame ||
+        function (callback) {
+            window.setTimeout(callback, 1000/60);
+        };
 })();
 
 // Draw to the canvas
 function renderCanvas() {
-  if (drawing) {
-    ctx.moveTo(lastPos.x, lastPos.y);
-    ctx.lineTo(mousePos.x, mousePos.y);
-    ctx.stroke();
-    lastPos = mousePos;
-  }
+    if (drawing) {
+        ctx.moveTo(lastPos.x, lastPos.y);
+        ctx.lineTo(mousePos.x, mousePos.y);
+        ctx.stroke();
+        lastPos = mousePos;
+    }
 }
 
 // Allow for animation
 (function drawLoop () {
-  requestAnimFrame(drawLoop);
-  renderCanvas();
+    requestAnimFrame(drawLoop);
+    renderCanvas();
 })();
 
 // Set up touch events for mobile, etc
 canvas.addEventListener("touchstart", function (e) {
-  mousePos = getTouchPos(canvas, e);
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent("mousedown", {
-    clientX: touch.clientX,
-    clientY: touch.clientY
+    mousePos = getTouchPos(canvas, e);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
     });
-  canvas.dispatchEvent(mouseEvent);
+    canvas.dispatchEvent(mouseEvent);
 }, false);
 
 canvas.addEventListener("touchend", function (e) {
-  var mouseEvent = new MouseEvent("mouseup", {});
-  canvas.dispatchEvent(mouseEvent);
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
 }, false);
 
 canvas.addEventListener("touchmove", function (e) {
-  var touch = e.touches[0];
-  var mouseEvent = new MouseEvent("mousemove", {
-    clientX: touch.clientX,
-    clientY: touch.clientY
-  });
-  canvas.dispatchEvent(mouseEvent);
+    var touch = e.touches[0];
+    var mouseEvent = new MouseEvent("mousemove", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+    });
+    canvas.dispatchEvent(mouseEvent);
 }, false);
 
 // Get the position of a touch relative to the canvas
 function getTouchPos(canvasDom, touchEvent) {
-  var rect = canvasDom.getBoundingClientRect();
-  return {
-    x: touchEvent.touches[0].clientX - rect.left,
-    y: touchEvent.touches[0].clientY - rect.top
-  };
+    var rect = canvasDom.getBoundingClientRect();
+    return {
+        x: touchEvent.touches[0].clientX - rect.left,
+        y: touchEvent.touches[0].clientY - rect.top
+    };
 }
 
 // Prevent scrolling when touching the canvas
 document.body.addEventListener("touchstart", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
 }, false);
+
 document.body.addEventListener("touchend", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
 }, false);
+
 document.body.addEventListener("touchmove", function (e) {
-  if (e.target == canvas) {
-    e.preventDefault();
-  }
+    if (e.target == canvas) {
+        e.preventDefault();
+    }
 }, false);
 
 // Clear the canvas
 function clean() {
-  canvas.width = canvas.width;
-  ctx.lineWidth = 20;
-  ctx.lineJoin = 'round';
-  ctx.lineCap = 'round';
+    canvas.width = canvas.width;
+    ctx.lineWidth = 20;
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+}
+
+// On call, send data to server
+function predict() {
+
+    // Convert canvas to DataURL
+    var dataURL = canvas.toDataURL();
+    var obj = {};
+    var arr = [];
+    var sorted_arr = [];
+    
+    // Ajax post to route /hook
+    $.ajax({ type: "POST",
+        url: "http://127.0.0.1:5000/hook",
+        data:{
+            imageBase64: dataURL
+        },
+        
+        // If success:
+        success: function(response){
+            
+            // Parse json
+            obj = JSON.parse(response);
+        
+            // Add all percentagens to an array
+            arr.push(obj.zero);
+            arr.push(obj.one);
+            arr.push(obj.two);
+            arr.push(obj.three);
+            arr.push(obj.four);
+            arr.push(obj.five);
+            arr.push(obj.six);
+            arr.push(obj.seven);
+            arr.push(obj.eight);
+            arr.push(obj.nine);
+            
+            // Get file name from obj.name
+            file = obj.name;
+            
+            // Reverse sort the array to get the winning prediction
+            sorted_arr = arr.sort().reverse();
+    
+            // First element is the winning probability
+            first_prob = sorted_arr[0];
+            
+            // Retrieve number key from percentage
+            const key1 = Object.keys(obj).find(key1 => obj[key1] === first_prob);
+            
+            // Round the percentage to 3 decimals
+            first_prob = first_prob.toFixed(3);
+            
+            // Redirect to results url with filename, key and probability values
+            const url = "http://127.0.0.1:5000/results/" + file + "/" + key1  + "/" + first_prob;
+            window.location.href = url;
+  
+        }
+    });
 }
--- web_app.py
+++ web_app.py
@@ -3,6 +3,7 @@ import base64
 import json
 import numpy as np
 import tensorflow as tf
+import cv2
 
 from flask import Flask, render_template, request
 from io import BytesIO
@@ -11,6 +12,58 @@ from PIL import Image
 app = Flask(__name__)
 
 # Default route: Render canvas for drawing
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def hello_world():
     return render_template('drawing_canvas.html')
+
+# Prediction on call
+@app.route("/hook", methods=['POST', 'GET'])
+def hook():
+
+    # Open image from Javascript
+    image_data = re.sub('^data:image/.+;base64,', '', request.form['imageBase64'])
+    im = Image.open(BytesIO(base64.b64decode(image_data))).convert("RGBA")
+
+    # Convert image background to white (otherwise it'll be transparent)
+    new_image = Image.new("RGBA", im.size, "WHITE")
+    new_image.paste(im, mask=im)
+    new_image.convert("RGB")
+    
+    # Save image as png
+    file = 'image.png'
+    filename = 'static/'+file
+    new_image.save(filename)
+
+    # Open image using python cv
+    image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    image = cv2.resize(image, (28, 28))
+    image = image.astype('float32')
+    image = image.reshape(1, 28, 28, 1)
+    image = 255-image
+    image /= 255
+
+    # Load model
+    digit_model = tf.keras.models.load_model('Digit_Recognition_Model.h5')
+
+    # Make prediction
+    prediction = digit_model.predict(image.reshape(1, 28, 28, 1), batch_size=1)
+
+    # Convert argmax values to percentages in a list
+    middle = prediction.tolist()
+    percentages = [item for sublist in middle for item in sublist]
+    percentages = [element * 100 for element in percentages]
+    percentages.append(file)
+
+    # Create dictionary percentagens of all possibilities
+    res = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'name']
+    zip_iterator = zip(res, percentages)
+    a_dict = dict(zip_iterator)
+
+    # Json dump the solution and send it back to Javascript
+    sol = json.dumps(a_dict)
+    return sol
+
+# Redirect page with the results
+@app.route("/results/<filen>/<result>/<prob>", methods=['POST', 'GET'])
+def results(filen, result, prob):
+    return render_template('digit_results.html', filen=filen, result=result, prob=prob)
